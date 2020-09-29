33b44226947b0b6c597a50a80336f46c57fda541
USER <user@AL01370192.local>
2020-09-30 01:59:49 +0900

    Graphql yoga, express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("working!")
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT,
+};
+
+const handleAppStart = () => console.log(`Server started ${PORT}`);
+
+app.start(appOptions, handleAppStart);
